aa247b1b55ff12ed2ce75d14265f71823a24723c
jeffw <jeffw@10.0.0.29>
2021-11-30 16:06:29 -0600

    Created Tank class and methods

--- src/zombiewar/Tank.java
+++ src/zombiewar/Tank.java
@@ -1,5 +1,39 @@
 package zombiewar;
 
 public class Tank {
+    private int health;
+    private int attack;
+    private String type;
+    private boolean alive;
     
+    private Tank(){
+        health = 30;
+        attack = 5;
+        type = "zombie";
+        alive = true;
+    }
+    public int getHealth(){
+        return health;
+    }
+    public int getAttack(){
+        return attack;
+    }
+    public String getType(){
+        return type;
+    }
+    public boolean getAlive(){
+        return alive;
+    }
+    public void setHealth(int health){
+        this.health = health;
+    }
+    public void setAttack(int attack){
+        this.attack = attack;
+    }
+    public void setAttack(String type){
+        this.type = type;
+    }
+    public void setAlive(boolean alive){
+        this.alive = alive;
+    }
 }
